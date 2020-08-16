6a76f61699dab844643a03acb1c176b4b8afd7b4
alexanderholder <alexholder96@gmail.com>
2020-08-16 22:55:43 +1000

    add workouts

---
+++ app/controllers/workouts_controller.rb
@@ -0,0 +1,49 @@
+class WorkoutsController < ApplicationController
+  def index
+    @workouts = Workout.all
+  end
+
+  def show
+    @workout = Workout.find(params[:id])
+  end
+
+  def new
+    @workout = Workout.new
+  end
+
+  def edit
+    @workout = Workout.find(params[:id])
+  end
+
+  def create
+    @workout = Workout.new(workout_params)
+
+    if @workout.save
+      redirect_to training_template_path(@workout.training_template_id)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @workout = Workout.find(params[:id])
+
+    if @workout.update(workout_params)
+      redirect_to @workout
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @workout = Workout.find(params[:id])
+    @workout.destroy
+
+    redirect_to training_template_path(@workout.training_template_id)
+  end
+
+  private
+  def workout_params
+    params.require(:workout).permit(:name, :day_number, :training_template_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :training_templates
-  resources :workouts
+  resources :training_templates do
+    resources :workouts, except: :index
+  end
+  post "workouts/new" => "workouts#create"
+
   resources :blocks
   resources :excercises
   resources :params
