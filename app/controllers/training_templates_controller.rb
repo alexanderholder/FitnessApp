893cb3f4913f7e117650040b5bad2a205ba94d5c
alexanderholder <alexander@tanda.co>
2020-08-16 13:00:36 +1000

    Create UI for training templates

---
+++ app/controllers/training_templates_controller.rb
@@ -0,0 +1,49 @@
+class TrainingTemplatesController < ApplicationController
+  def index
+    @training_templates = TrainingTemplate.all
+  end
+
+  def show
+    @training_template = TrainingTemplate.find(params[:id])
+  end
+
+  def new
+    @training_template = TrainingTemplate.new
+  end
+
+  def edit
+    @training_template = TrainingTemplate.find(params[:id])
+  end
+
+  def create
+    @training_template = TrainingTemplate.new(training_template_params)
+
+    if @training_template.save
+      redirect_to @training_template
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @training_template = TrainingTemplate.find(params[:id])
+
+    if @training_template.update(training_template_params)
+      redirect_to @training_template
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @training_template = TrainingTemplate.find(params[:id])
+    @training_template.destroy
+
+    redirect_to training_templates_path
+  end
+
+  private
+  def training_template_params
+    params.require(:training_template).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :training_templates
+  resources :workouts
+  resources :blocks
+  resources :excercises
+  resources :params
+
+  get "/" => redirect("/training_templates"), as: "root"
 end
