d9bc3ad05228f55e8f7bf0cab9e39e60cd4db24e
alexanderholder <alexholder96@gmail.com>
2020-08-22 23:35:19 +1000

    finish devise and some app clean up

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 
   has_many :training_templates, inverse_of: :user, dependent: :destroy
 
-  validates :name, :presence => true
+  # validates :name, :presence => true
 end
