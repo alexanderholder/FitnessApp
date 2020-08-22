fb834132885eb437ca374b0e46e05fd8ae707bf2
alexanderholder <alexholder96@gmail.com>
2020-08-22 23:03:22 +1000

    devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :trackable, :lockable, :timeoutable,
+         :confirmable
+
   has_many :training_templates, inverse_of: :user, dependent: :destroy
 
   validates :name, :presence => true
--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,13 @@ module FitnessApp
     config.load_defaults 5.2
     config.autoload_paths += %W(#{config.root}/lib)
 
+    config.time_zone = "Brisbane"
+
+    # omniauth requirements https://github.com/omniauth/omniauth
+    # config.session_store :cookie_store, key: '_interslice_session'
+    # config.middleware.use ActionDispatch::Cookies # Required for all session management
+    # config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
