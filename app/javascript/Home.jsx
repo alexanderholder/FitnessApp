1c54c05e5586c219ea1c52c4cc3738a1414c4dbf
alexanderholder <alexholder96@gmail.com>
2020-09-07 18:05:23 +1000

    Add profile selector

--- app/javascript/Home.jsx
+++ app/javascript/Home.jsx
@@ -3,15 +3,19 @@ import React from "react";
 import Calendar from "./components/calendar/Calendar";
 import Sidebar from "./components/sidebar/Sidebar";
 import TemplateSearch from "./components/templates/Search";
+import Navbar from "./components/navbar/Navbar";
 
 export default () => (
   <div className="app">
+    <div className="float-right">
+      <Navbar />
+    </div>
     <div className="side-bar">
       <Sidebar />
     </div>
     <div className="calendar">
       <TemplateSearch />
-      {/* <h2>Program Calendar</h2> */}
+      {/* <h2>Program Calendar</h2> TODO confirm can remove this in favour of searchbar */}
       <Calendar />
     </div>
   </div>
---
+++ app/javascript/components/navbar/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LetterAvatar from "./components/LetterAvatar";
+
+export default () => (
+  <div>
+    <LetterAvatar />
+  </div>
+)
