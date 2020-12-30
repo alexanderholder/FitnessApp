5c916cab22a72ba6f929b3c83c7e38de44ec7308
alexanderholder <alexholder96@gmail.com>
2020-12-30 11:17:49 +1000

    add initial state to controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,11 +1,24 @@
 class WorkoutsController < ApplicationController
-  def json
-    render json: Oj.dump(Workout.all.to_json)
+  def index
+    @initial_state = initial_state
+    respond_to do |format|
+      format.html { render "index" }
+    end
   end
 
-  def index
-    # TODO
-    @workouts = Workout.all
+  def initial_state
+    training_templates = TrainingTemplate.where(user: current_user.id)
+    workouts = Workout.where(training_template_id: training_templates.ids)
+    blocks = Block.where(workout_id: workouts.ids)
+    excercises = Excercise.where(block_id: blocks.ids)
+
+    {
+      user: { user_id: current_user.id, signed_in: true, selected_template: training_templates.first.id },
+      templates: training_templates,
+      workouts: workouts,
+      blocks: blocks,
+      excercises: excercises
+    }
   end
 
   def show
