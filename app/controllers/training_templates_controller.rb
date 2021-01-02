aa18b91636afb2d8089763031002d86dbf822d25
alexanderholder <alexholder96@gmail.com>
2021-01-02 14:02:31 +1000

    delete old haml code

--- app/controllers/training_templates_controller.rb
+++ app/controllers/training_templates_controller.rb
@@ -1,21 +1,4 @@
 class TrainingTemplatesController < ApplicationController
-  def index
-    @training_templates = TrainingTemplate.where(user_id: current_user.id)
-  end
-
-  def show
-    @training_template = TrainingTemplate.find(params[:id])
-    @workouts = @training_template.workouts
-  end
-
-  def new
-    @training_template = TrainingTemplate.new
-  end
-
-  def edit
-    @training_template = TrainingTemplate.find(params[:id])
-  end
-
   def create
     @training_template = TrainingTemplate.new(training_template_params)
     @training_template.user_id = current_user.id
@@ -24,16 +7,6 @@ class TrainingTemplatesController < ApplicationController
       render json: @training_template.attributes.as_json
     else
       head :bad_request
-    end
-  end
-
-  def update
-    @training_template = TrainingTemplate.find(params[:id])
-
-    if @training_template.update(training_template_params)
-      redirect_to @training_template
-    else
-      render 'edit'
     end
   end
 
