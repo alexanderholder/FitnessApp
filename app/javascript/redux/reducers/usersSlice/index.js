a05dbf1afa99188b6ba669ae4d83d3794ad0da9b
alexanderholder <alexholder96@gmail.com>
2021-01-03 09:31:40 +1000

    local storage for template

--- app/javascript/redux/reducers/usersSlice/index.js
+++ app/javascript/redux/reducers/usersSlice/index.js
@@ -1,6 +1,9 @@
+import LocalStorage from 'javascript/localStorage'
+
 export default function appReducer(state = null, action) {
   switch (action.type) {
     case 'user/temaplteChanged': {
+      LocalStorage.setItem('current_template_id', action.payload)
       return {
         ...state,
         selected_template: action.payload
