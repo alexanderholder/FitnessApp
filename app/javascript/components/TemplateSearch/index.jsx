94a8ac99751d13972db9460a7e1f63a1ef5c938f
alexanderholder <alexholder96@gmail.com>
2021-01-17 13:51:54 +1000

    short hand imports

--- app/javascript/components/TemplateSearch/index.jsx
+++ app/javascript/components/TemplateSearch/index.jsx
@@ -5,17 +5,17 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import * as Selectors from 'javascript/redux/selectors'
 import * as Actions from 'javascript/redux/reducers/templatesSlice'
-import TextField from '@material-ui/core/TextField'
-import Dialog from '@material-ui/core/Dialog'
-import DialogTitle from '@material-ui/core/DialogTitle'
-import DialogContent from '@material-ui/core/DialogContent'
-import DialogActions from '@material-ui/core/DialogActions'
-import Button from '@material-ui/core/Button'
 import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete'
-import IconButton from '@material-ui/core/IconButton'
 import DeleteIcon from '@material-ui/icons/Delete'
 import parse from 'autosuggest-highlight/parse'
 import match from 'autosuggest-highlight/match'
+import { TextField,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  IconButton } from '@material-ui/core'
 
 const TemplateSearch = props => {
   const [open, toggleOpen] = useState(false)
