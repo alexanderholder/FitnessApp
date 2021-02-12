251dd750751464b0bf165fad383dff1c120d9216
alexanderholder <alexholder96@gmail.com>
2021-02-12 13:52:35 +1000

    remove drawer

--- app/javascript/components/Calendar/components/Workout/components/ExcerciseForm/index.jsx
+++ app/javascript/components/Calendar/components/Workout/components/ExcerciseForm/index.jsx
@@ -11,7 +11,6 @@ import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField, T
 import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete'
 import parse from 'autosuggest-highlight/parse'
 import match from 'autosuggest-highlight/match'
-import Drawer from './components/Drawer'
 
 function ExcerciseForm(props) {
   const { movement, measurement_value } = props.excercise
@@ -180,7 +179,121 @@ function ExcerciseForm(props) {
               value={setsReps}
               width='25'
               // helperText='Some important text' TODO
+              // setsRepsSchemeList
             />
+            {/* <Autocomplete
+              id="sets-and-reps"
+              onChange={(event, newValue) => {
+                if (typeof newValue === 'string') {
+                  // timeout to avoid instant validation of the dialog's form.
+                  setTimeout(() => {
+                    toggleOpen(true)
+                    setDialogValue({
+                      title: newValue,
+                      category: '',
+                    })
+                  })
+                } else if (newValue && newValue.inputValue) {
+                  toggleOpen(true)
+                  setDialogValue({
+                    title: newValue.inputValue,
+                    category: '',
+                  })
+                } else {
+                  props.updateExcercise({ movement: newValue.title })
+                  setName(newValue.title)
+                }
+              }}
+              selectOnFocus
+              clearOnBlur
+              handleHomeEndKeys
+              value={name}
+              options={excerciseList}
+              filterOptions={(options, params) => {
+                const filter = createFilterOptions()
+                const filtered = filter(options, params)
+                // Suggest the creation of a new value
+                if (params.inputValue !== '') {
+                  filtered.push({
+                    inputValue: params.inputValue,
+                    title: `Add "${params.inputValue}"`,
+                  })
+                }
+                return filtered
+              }}
+              getOptionLabel={(option) => {
+                // Value selected with enter, right from the input
+                if (typeof option === 'string') {
+                  return option
+                }
+                // Add "xxx" option created dynamically
+                if (option.inputValue) {
+                  return option.inputValue
+                }
+                // Regular option
+                return option.title
+              }}
+              disableClearable
+              forcePopupIcon={false}
+              renderInput={(params) => (
+                <TextField {...params}
+                  autoFocus={true}
+                  label='Excercise Name'
+                  margin="dense"
+                  size='small'
+                  style={{ width: 300, textOverlow:'none' }}
+                  variant="outlined"
+                />
+              )}
+              renderOption={(option, { inputValue }) => {
+                const matches = match(option.title, inputValue)
+                const parts = parse(option.title, matches)
+
+                return (
+                  <div>
+                    {parts.map((part, index) => (
+                      <span key={index} style={{ fontWeight: part.highlight ? 700 : 400 }}>
+                        {part.text}
+                      </span>
+                    ))}
+                  </div>
+                )
+              }}
+            />
+            <Dialog open={open} aria-labelledby="add-excercise">
+              <form onSubmit={handleSubmit}>
+                <DialogTitle id="add-excercise">Add a new excercise</DialogTitle>
+                <DialogContent>
+                  <TextField
+                    autoFocus
+                    id="excercise"
+                    label="excercise"
+                    margin="dense"
+                    onChange={(event) => setDialogValue({ ...dialogValue, title: event.target.value })}
+                    required={true}
+                    type="text"
+                    value={dialogValue.title}
+                  />
+                  <TextField
+                    id="category"
+                    label="category"
+                    margin="dense"
+                    onChange={(event) => setDialogValue({ ...dialogValue, category: event.target.value })}
+                    required={true}
+                    type="text"
+                    value={dialogValue.category}
+                  />
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose} color="primary">
+                    Cancel
+                  </Button>
+                  <Button type="submit" color="primary">
+                    Add
+                  </Button>
+                </DialogActions>
+              </form>
+            </Dialog> */}
           </td>
           <td>
             <IconButton onClick={() => props.setShowExcerciseDetails(props.excerciseId) }>
