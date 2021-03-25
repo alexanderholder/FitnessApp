3784bb105aea04a7fedbd616cf813a413a2e8b50
alexanderholder <alexholder96@gmail.com>
2021-03-25 18:23:57 +1000

    fix sort order

--- app/models/excercise.rb
+++ app/models/excercise.rb
@@ -1,3 +1,20 @@
+# typed: strict
 class Excercise < ApplicationRecord
+  extend T::Sig
+
   belongs_to :block, inverse_of: :excercises, optional: false
+
+  before_validation :confirm_or_create_sort_order
+
+  sig { returns(Integer) }
+  def confirm_or_create_sort_order
+    return self.sort_order if self.sort_order.present?
+
+    max_sort_order = self.block.excercises
+      .map(&:sort_order)
+      .compact
+      .max || 0
+
+    max_sort_order + 1
+  end
 end
