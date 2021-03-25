c0be9a6136ad1d30c08180715f19e9588c343b80
alexanderholder <alexholder96@gmail.com>
2021-03-25 16:18:16 +1000

    remove local storage from initial state

--- app/javascript/redux/store.js
+++ app/javascript/redux/store.js
@@ -2,7 +2,6 @@ import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
-import LocalStorage from 'javascript/localStorage';
 
 const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
 
