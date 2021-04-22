cfba5d3bee93fecd4448901ec96494906edaf4f2
alexanderholder <alexholder96@gmail.com>
2021-04-22 22:56:07 +1000

    yarn updates

--- app/javascript/redux/store.js
+++ app/javascript/redux/store.js
@@ -1,15 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
-
-const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
 
 let preloadedState
 if (window.__INITIAL_STATE__) {
   preloadedState = JSON.parse(window.__INITIAL_STATE__)
 }
 
-const store = createStore(rootReducer, preloadedState, composedEnhancer)
+const store = createStore(rootReducer, preloadedState, applyMiddleware(thunkMiddleware))
 
 export default store
