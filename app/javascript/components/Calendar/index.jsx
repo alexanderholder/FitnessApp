1840e2706c19d9417baa97c4c53b707edf740fed
alexanderholder <alexholder96@gmail.com>
2021-04-28 22:28:56 +1000

    search by day

--- app/javascript/components/Calendar/index.jsx
+++ app/javascript/components/Calendar/index.jsx
@@ -4,55 +4,105 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import * as Selectors from 'javascript/redux/selectors'
 import Day from './Day'
+import TextField from '@material-ui/core/TextField';
 
 const NUMBER_OF_DAYS_IN_WEEK = 7
 const daysOfTheWeek = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
 
-const Week = (props) => {
-  let cells = []
-  for (let i = 0; i < NUMBER_OF_DAYS_IN_WEEK; i++) {
-    const workoutDayNumber = (i + 1) + (NUMBER_OF_DAYS_IN_WEEK * props.weekNumber)
+function Calendar(props) {
+  const [search, setSearch] = React.useState('')
 
-    cells.push(
-      <Day
-        key = {workoutDayNumber}
-        dayNumber={workoutDayNumber}
-      />
-    )
+  const Week = ({weekNumber, day}) => {
+    const cells = []
+    let i
+    if (day) {
+      const dayNumber = daysOfTheWeek.indexOf(day)
+
+      for (i = 0; i < props.templateLength; i++) {
+        const workoutDayNumber = dayNumber + 1 + (i * NUMBER_OF_DAYS_IN_WEEK)
+
+        cells.push(
+          <Day
+            key = {workoutDayNumber}
+            dayNumber={workoutDayNumber}
+          />
+        )
+      }
+    }
+    else {
+      for (i = 0; i < NUMBER_OF_DAYS_IN_WEEK; i++) {
+        const workoutDayNumber = (i + 1) + (NUMBER_OF_DAYS_IN_WEEK * weekNumber)
+
+        cells.push(
+          <Day
+            key = {workoutDayNumber}
+            dayNumber={workoutDayNumber}
+          />
+        )
+      }
+    }
+
+    return <tr>{cells}</tr>
   }
 
-  return <tr>{cells}</tr>
-}
-
-const Calendar = (props) => {
   const TableRows = () => {
     const rows = []
-    for(var i = 0; i < props.templateLength; i++) {
+
+    if (daysOfTheWeek.includes(search)) {
       rows.push(
         <Week
-          key={i + 1}
-          weekNumber={i}
+          key={search}
+          day={search}
         />
       )
     }
+    else {
+      for(var i = 0; i < props.templateLength; i++) {
+        rows.push(
+          <Week
+            key={i + 1}
+            weekNumber={i}
+          />
+        )
+      }
+    }
+
     return rows
   }
 
+  const TableHeader = () => {
+    if (daysOfTheWeek.includes(search)) {
+      let i
+      const headerRow = []
+      for (i = 0; i < props.templateLength; i++) {
+        headerRow.push(<th key={`${search} i`} className='header-cell'>{search}</th>)
+      }
+      return headerRow
+    }
+    else {
+      return daysOfTheWeek.map(day => <th key={day} className='header-cell'>{day}</th>)
+    }
+  }
+
   return (
+    <>
+    <TextField
+      label="Search"
+      style={{ boder: 5, paddingRight: 5, }}
+      value={search}
+      onChange={(event) => setSearch(event.target.value)}
+    />
     <table className='calendar'>
       <thead>
         <tr>
-          {daysOfTheWeek.map(day =>
-            <th key={day} className='header-cell'>
-              {day}
-            </th>
-          )}
+          <TableHeader />
         </tr>
       </thead>
       <tbody>
         <TableRows />
       </tbody>
     </table>
+    </>
   )
 }
 
