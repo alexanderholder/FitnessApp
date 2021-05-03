7a64a7fae73e4be9e2b9cdb3d4a0298fef0e2c95
alexanderholder <alexholder96@gmail.com>
2021-05-03 13:56:58 +1000

    Fix webpacker and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [],
+  purge: ['./app/javascript/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
